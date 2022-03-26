b4405cf0867287fdb810112e74466939dae83ac2
belaidimohamed <mohamedbelaidi408@gmail.com>
2022-03-26 06:12:46 +0100

    base uri changed

--- frontend/src/app/global-constant.ts
+++ frontend/src/app/global-constant.ts
@@ -1,4 +1,4 @@
 export class GlobalConstants {
   // public static apiURL: string = 'http://192.168.1.17:8000/';
-  public static apiURL: string = 'http://mohamedyahoo.pythonanywhere.com/';
+  public static apiURL: string = 'http://mohamedyahoo88.pythonanywhere.com/';
 }
